12fc3f6e1f1e6651b4975199b08fd734b570eb59
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2026-05-10 23:07:54 +0500

    feat(hero): add HeroSection with layout and DecorativeIcons component

---
+++ src/pages/main/components/DecorativeIcons.js
@@ -0,0 +1,16 @@
+export function DecorativeIcons() {
+  return `
+    <!-- leaf -->
+    <div class="absolute -top-10 right-0 opacity-30 xl:top-5 animate-movingY">
+      <i class="ri-leaf-line text-6xl text-yellow-500"></i>
+    </div>
+    <!-- flower -->
+    <div class="absolute bottom-0 left-0 opacity-30 xl:bottom-12 animate-rotating">
+      <i class="ri-flower-line text-6xl text-yellow-500"></i>
+    </div>
+    <!-- plant -->
+    <div class="hidden absolute -top-8 -left-5 opacity-30 lg:block animate-scalingUp">
+      <i class="ri-plant-line text-6xl text-yellow-500"></i>
+    </div>
+  `
+}
