289d6edb740ba171ff841fc971734e51a2d885ed
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2026-05-12 16:15:14 +0500

    feat(about): integrate AboutSection into page Index

--- src/pages/main/Index.js
+++ src/pages/main/Index.js
@@ -1,11 +1,13 @@
 import HeroSection from './sections/HeroSection'
 import ServicesSection from './sections/ServicesSection'
+import AboutSection from './sections/AboutSection'
 
 export function Index() {
   return `
     <main>
       ${HeroSection()}
       ${ServicesSection()}
+      ${AboutSection()}
     </main>
   `
 }
