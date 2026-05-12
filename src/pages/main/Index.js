ae96bc331a71a3de891165f1f3260b225d843127
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2026-05-12 15:41:36 +0500

    feat(services): integrate ServicesSection into page Index

--- src/pages/main/Index.js
+++ src/pages/main/Index.js
@@ -1,9 +1,11 @@
 import HeroSection from './sections/HeroSection'
+import ServicesSection from './sections/ServicesSection'
 
 export function Index() {
   return `
     <main>
       ${HeroSection()}
+      ${ServicesSection()}
     </main>
   `
 }
