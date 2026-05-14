ad937aa9cceb1c66668adde624915ccc7e1a5437
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2026-05-14 11:58:39 +0500

    feat(footer): add footer sub-components (logo, links, contact, copyright, floral, social)

---
+++ src/components/footer/Copyright.js
@@ -0,0 +1,9 @@
+export function Copyright() {
+  return `
+    <div class="copy__right container mx-auto p-8">
+        <p class="text-center mt-10 opacity-50">
+          Copyright &copy; 2026 LightCode. All rights reserved.
+        </p>
+    </div>
+  `
+}
---
+++ src/components/footer/FooterLogo.js
@@ -0,0 +1,10 @@
+export function FooterLogo() {
+  return `
+    <div class="text-7xl text-center text-green-700 inline-block">
+      <div>
+        <i class="ri-leaf-fill"></i>
+        <p class="font-[Lobster] text-xl sm:text-2xl">IndorePlants.</p>
+      </div>
+    </div>
+  `
+}
