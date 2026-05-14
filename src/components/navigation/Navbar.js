6981a8aa252caabd183ef273e417fc8f06852d5e
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2026-05-14 17:54:16 +0500

    fix(navbar): fix nav-menu overflow blocking page interactions on desktop

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -6,7 +6,7 @@ export function Navbar() {
       <div class="font-[Lobster] text-3xl">IndorePlants.</div>
       <div
         id="nav-menu"
-        class="absolute overflow-y-auto h-screen py-60 top-0 -left-full min-x-[80px] w-full bg-green-950/80 flex items-center justify-center backdrop-blur-sm duration-300 overflow-hidden lg:static lg:min-h-fit lg:bg-transparent lg:w-auto lg:backdrop-blur-none"
+        class="absolute overflow-y-auto h-screen py-60 top-0 -left-full min-x-[80px] w-full bg-green-950/80 flex items-center justify-center backdrop-blur-sm duration-300 overflow-hidden lg:static lg:h-auto lg:w-auto lg:min-h-fit lg:bg-transparent lg:backdrop-blur-none lg:py-0 lg:overflow-visible"
       >
         <ul class="flex flex-col items-center gap-x-8 gap-y-12 lg:flex-row">
           <li><a href="#hero" class="nav-link active">Home</a></li>
